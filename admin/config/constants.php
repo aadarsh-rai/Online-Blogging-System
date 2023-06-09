990bb92485ebe9a8782d3cc72778595c85126e59
My Name <Aadarsh.Rai.AR@gmail.com>
2023-06-09 19:22:02 +0545

    Connecting to the database

--- admin/config/constants.php
+++ admin/config/constants.php
@@ -1,3 +1,7 @@
 <?php
 define('ROOT_URL', 'http://localhost/blogging/');
+define('DB_HOST' ,'localhost'); //Points to the local server
+define('DB_USER' ,'root'); //User name for connecting the database
+define('DB_PASS' ,''); //Password for connection to the database
+define('DB_NAME' ,'db_blog') //Name of the database
 ?> 
