232ef13e9150461709f4dee62cb4ea268ebfd1e3
Aadarsh Rai <Aadarsh.Rai.AR@gmail.com>
2023-06-17 21:41:56 +0545

    creating posts table in the database & implementing foreign key

--- database/db_blog.sql
+++ database/db_blog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 17, 2023 at 04:59 PM
+-- Generation Time: Jun 17, 2023 at 05:51 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -24,85 +24,52 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `categories`
+-- Table structure for table `posts`
 --
 
-CREATE TABLE `categories` (
+CREATE TABLE `posts` (
   `id` int(10) UNSIGNED NOT NULL,
-  `title` varchar(50) NOT NULL,
-  `description` text NOT NULL
+  `title` varchar(100) NOT NULL,
+  `body` text NOT NULL,
+  `thumbnail` varchar(255) NOT NULL,
+  `date_time` timestamp NOT NULL DEFAULT current_timestamp(),
+  `category_id` int(10) UNSIGNED DEFAULT NULL,
+  `author_id` int(10) UNSIGNED NOT NULL,
+  `is_featured` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `categories`
---
-
-INSERT INTO `categories` (`id`, `title`, `description`) VALUES
-(2, 'Travel', 'This is a travel category'),
-(3, 'Anime', 'This is a  anime category'),
-(4, 'Science &amp; Technology', 'This is a science &amp; technology category'),
-(5, 'Wildlife', 'This is a wildlife category'),
-(6, 'Food ', 'This is a food category'),
-(7, 'Uncategorized', 'This is an uncategorized category'),
-(8, 'Food ', 'This is a food category');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `firstname` varchar(30) NOT NULL,
-  `lastname` varchar(30) NOT NULL,
-  `username` varchar(30) NOT NULL,
-  `email` varchar(60) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `avatar` varchar(100) NOT NULL,
-  `is_admin` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `firstname`, `lastname`, `username`, `email`, `password`, `avatar`, `is_admin`) VALUES
-(1, 'Sponge', 'Bob', 'Spongebob', 'Spongebob@gmail.com', '$2y$10$GpmcpSQhHkHX3LI9/ycdYOzG/xtoB1v1QXTQEcsRSrbZv13hTuk1K', '1686382167avatar-5.jpg', 1),
-(2, 'Mob ', 'Psycho', 'Kageyama', 'Kageyama@gmail.com', '$2y$10$qyzXEe9F1HFh4iiuPwQ4ge7wOFFFFbj19DCulB8eP2mbLM/3kNq5O', '1686413122avatar-9.jpg', 0),
-(3, 'Squid', 'ward', 'Squidward', 'Squidward123@gmail.com', '$2y$10$VNtmaU/HYEyw2/2WBkp2VOyUBe/cwiieWjiUmvkKVMbFT8aVQq9MW', '1686644518avatar-1.jpg', 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `categories`
+-- Indexes for table `posts`
 --
-ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `foriegn_key_blog_category` (`category_id`),
+  ADD KEY `foriegn_key_author` (`author_id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `categories`
+-- AUTO_INCREMENT for table `posts`
 --
-ALTER TABLE `categories`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `posts`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `users`
+-- Constraints for dumped tables
 --
-ALTER TABLE `users`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `foriegn_key_author` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `foriegn_key_blog_category` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE SET NULL;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
